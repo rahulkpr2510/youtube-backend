2b5223ca174549f9debbad804fac90de21dac5e5
Rahul Kapoor <rahulkpr1972@gmail.com>
2024-11-20 09:21:18 +0530

    all setup configured

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,23 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
+    const {fullName, email, username, password} = req.body
+
+    if(
+        [fullName, email,username,password].some((field) => field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{username}, {email}]
     })
+
+    if(existedUser){
+        throw new ApiError(409, "User with email or username already exists")
+    }
 })
 
 export {registerUser}
