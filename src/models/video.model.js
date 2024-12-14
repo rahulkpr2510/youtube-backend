929fec6d8d1ed8be1f9566886fe4792bf96bc587
Rahul Kapoor <rahulkpr1972@gmail.com>
2024-12-14 12:19:15 +0530

    add getUserChannelProfile API and update

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -10,7 +10,7 @@ const videoSchema = new Schema({
         type: String,
         required: [true, "Thumbnail is required"]
     },
-    onwer: {
+    owner: {
         type: Schema.Types.ObjectId,
         ref: 'User',
     },
